e262d44527f83bc0ceeb9dcfd101109bf0b8c260
Атанас Апостолов <ATApostolov21@codingburgas.bg>
2023-03-23 00:35:09 +0200

    Fix the bug with flickering the window and make some changes on the positions of the objects

--- 2223-9th-grade-sprint-math-games/game.cpp
+++ 2223-9th-grade-sprint-math-games/game.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "Header.h"
-#define MAX_INPUT_CHARS     9
+
+#define MAX_INPUT_CHARS 9
 
 void game()
 {
@@ -9,110 +10,93 @@ void game()
 
     InitWindow(screenWidth, screenHeight, "Game");
 
-    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-
-    Image image = LoadImage("../assets/c4.png");     // Loaded in CPU memory (RAM)
+    // Load texture
+    Image image = LoadImage("../assets/c4.png");
     ImageResize(&image, 200, 250);
-    Texture2D texture = LoadTextureFromImage(image);          // Image converted to texture, GPU memory (VRAM)
+    Texture2D texture = LoadTextureFromImage(image);
     UnloadImage(image);
 
-    char name[MAX_INPUT_CHARS + 1] = "\0";      // NOTE: One extra space required for null terminator char '\0'
+    // Set up text box
+    char name[MAX_INPUT_CHARS + 1] = "\0";
     int letterCount = 0;
+    Rectangle textBox = { screenWidth / 2.0f - 100, 250, 225, 50 };
 
-    Rectangle textBox = { screenWidth / 2.0f - 100, 180, 225, 50 };
-    bool mouseOnText = false;
-
-    int framesCounter = 0;
-
+    // Timer
+    float time = 0.0f;
+    float timeLeft = 0.0f;
+    bool timerStarted = false;
 
     while (!WindowShouldClose()) {
 
-        if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOnText = true;
-        else mouseOnText = false;
-
-        if (mouseOnText)
-        {
-            // Set the window's cursor to the I-Beam
-            SetMouseCursor(MOUSE_CURSOR_IBEAM);
-
-            // Get char pressed (unicode character) on the queue
-            int key = GetCharPressed();
-
-            // Check if more characters have been pressed on the same frame
-            while (key > 0)
-            {
-                // NOTE: Only allow keys in range [32..125]
-                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
-                {
-                    name[letterCount] = (char)key;
-                    name[letterCount + 1] = '\0'; // Add null terminator at the end of the string.
-                    letterCount++;
-                }
-
-                key = GetCharPressed();  // Check next character in the queue
-            }
-
-            if (IsKeyPressed(KEY_BACKSPACE))
-            {
-                letterCount--;
-                if (letterCount < 0) letterCount = 0;
-                name[letterCount] = '\0';
-            }
-        }
+        // Handle text input
+        bool mouseOnText = CheckCollisionPointRec(GetMousePosition(), textBox);
+        if (mouseOnText) SetMouseCursor(MOUSE_CURSOR_IBEAM);
         else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
-        if (mouseOnText) framesCounter++;
-        else framesCounter = 0;
-        //----------------------------------------------------------------------------------
+        int key = GetCharPressed();
+        while (key > 0) {
+            if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS)) {
+                name[letterCount] = (char)key;
+                name[letterCount + 1] = '\0';
+                letterCount++;
+            }
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE)) {
+            letterCount--;
+            if (letterCount < 0) letterCount = 0;
+            name[letterCount] = '\0';
+        }
+
+        // Timer
+        if (timerStarted) {
+            float currentTime = (float)GetTime();
+            timeLeft = 60.0f - (currentTime - time);
+
+            if (timeLeft < 0.0f) {
+                timeLeft = 0.0f;
+                timerStarted = false;
+                CloseWindow();
+            }
+        }
 
         // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        DrawText("PLACE MOUSE OVER INPUT BOX!", 240, 140, 20, GRAY);
+        // Draw texture
+        DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 50, WHITE);
 
-        DrawRectangleRec(textBox, LIGHTGRAY);
-        if (mouseOnText) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height - 40, RED);
-        else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
+        // Draw text box
+        DrawRectangleRec(textBox, WHITE);
+        DrawRectangleLinesEx(textBox, 2, BLACK);
 
-        DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
+        // Draw input text
+        DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 40, BLACK);
 
-        DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, MAX_INPUT_CHARS), 315, 250, 20, DARKGRAY);
+        // Draw input counter
+        DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, MAX_INPUT_CHARS), screenWidth - 225, screenHeight - 50, 20, WHITE);
 
-        if (mouseOnText)
-        {
-            if (letterCount < MAX_INPUT_CHARS)
-            {
-                // Draw blinking underscore char
-                if (((framesCounter / 20) % 2) == 0) DrawText("_", (int)textBox.x + 8 + MeasureText(name, 40), (int)textBox.y + 12, 40, MAROON);
-            }
-            else DrawText("Press BACKSPACE to delete chars...", 230, 300, 20, GRAY);
+        // Draw timer
+        if (timerStarted) {
+            DrawText(TextFormat("Time remaining: %.2f", timeLeft), 10, 10, 20, RAYWHITE);
+        }
+        else {
+            DrawText("Press SPACE to start the game", 10, 10, 20, RAYWHITE);
         }
 
         EndDrawing();
 
-        bool keyPressed = false;
-        int key = GetKeyPressed();
-
-        if ((key >= 32) && (key <= 126)) keyPressed = true;
-
-        keyPressed;
-
-
-
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2, WHITE);
-
-
-
-        EndDrawing();
-
-
-
+        // Handle game start
+        if (IsKeyPressed(KEY_SPACE)) {
+            time = (float)GetTime();
+            timerStarted = true;
+        }
     }
+
+    // Unload resources
+    UnloadTexture(texture);
+    CloseWindow();
 }
