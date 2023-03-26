773b1f6b6b99be35055c9f486be63d92b3895336
GYFilipov21 <GYFilipov21@codingburgas.bg>
2023-03-26 19:23:16 +0300

    Add options menu.

--- 2223-9th-grade-sprint-math-games/menu.cpp
+++ 2223-9th-grade-sprint-math-games/menu.cpp
@@ -6,14 +6,15 @@ void menu()
 
     const int screenWidth = 800;
     const int screenHeight = 450;
-
+    bool inOptions = 0;
+    float timeInGame = 60.0f;
     InitWindow(screenWidth, screenHeight, "Menu");
 
     Image image = LoadImage("logoGame-removebg-preview.png");
     ImageResize(&image, 450, 380);
     Texture2D texture = LoadTextureFromImage(image);
     Texture2D background = LoadTexture("../assets/menu-bg.png");
-    
+   
 
     UnloadImage(image);
 
@@ -22,53 +23,102 @@ void menu()
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(BLACK);
         DrawTexture(background, 0, 0, WHITE);
-        DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 50, WHITE);
+       
 
-        // Start Game Button
-        if (CheckCollisionPointRec(GetMousePosition(), { screenWidth / 2 - 100, 300, 200, 50 }))
+
+
+        ///////////////// IN MAIN MENU
+        if (!(inOptions))
         {
-            DrawText("Start Game", screenWidth / 2 - MeasureText("Start Game", 40) / 2, 300, 40, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            DrawTexture(texture, screenWidth / 2 - texture.width / 2, screenHeight / 2 - texture.height / 2 - 50, WHITE);
+            if (CheckCollisionPointRec(GetMousePosition(), { screenWidth / 2 - 100, 300, 200, 50 }))
             {
-                CloseWindow();
-                countdown();
+                DrawText("Start Game", screenWidth / 2 - MeasureText("Start Game", 40) / 2, 300, 40, WHITE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    CloseWindow();
+                    countdown();
+                }
+            }
+            else
+            {
+                DrawText("Start Game", screenWidth / 2 - MeasureText("Start Game", 20) / 2, 300, 20, RED);
+            }
+
+            // Options Button
+            if (CheckCollisionPointRec(GetMousePosition(), { screenWidth / 2 - 100, 360, 200, 50 }))
+            {
+                DrawText("Options", screenWidth / 2 - MeasureText("Options", 40) / 2, 360, 40, WHITE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    inOptions = 1;
+                }
+            }
+            else
+            {
+                DrawText("Options", screenWidth / 2 - MeasureText("Options", 20) / 2, 360, 20, RED);
+            }
+
+            // Exit Button
+            if (CheckCollisionPointRec(GetMousePosition(), { 0, screenHeight - 40, 200, 50 }))
+            {
+                DrawText("Exit", 20, screenHeight - 40, 40, WHITE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    CloseWindow();
+                }
+            }
+            else
+            {
+                DrawText("Exit", 20, screenHeight - 40, 20, RED);
             }
         }
+
+
+
+
+
+        ///////////////// IN OPTIONS
         else
         {
-            DrawText("Start Game", screenWidth / 2 - MeasureText("Start Game", 20) / 2, 300, 20, RED);
-        }
-
-        // Options Button
-        if (CheckCollisionPointRec(GetMousePosition(), { screenWidth / 2 - 100, 360, 200, 50 }))
-        {
-            DrawText("Options", screenWidth / 2 - MeasureText("Options", 40) / 2, 360, 40, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            if (CheckCollisionPointRec(GetMousePosition(), { screenWidth / 2 - 100, 100, 200, 60 }))
             {
-                CloseWindow();
+                DrawText(TextFormat("Time: %.0fs",timeInGame), screenWidth / 2 - 130, 100, 60, WHITE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    if (timeInGame < 300)
+                    {
+                        timeInGame += 10;
+                    }
+                    else if (timeInGame >= 300)
+                    {
+                        timeInGame = 30;
+                    }
+                }
+            }
+            else
+            {
+                DrawText(TextFormat("Time: %.0fs", timeInGame), screenWidth / 2 - 130, 100, 60, RED);
+            }
+
+
+            // Back Button
+            if (CheckCollisionPointRec(GetMousePosition(), { 0, screenHeight - 40, 200, 50 }))
+            {
+                DrawText("Back", 20, screenHeight - 40, 40, WHITE);
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    inOptions = 0;
+                }
+            }
+            else
+            {
+                DrawText("Back", 20, screenHeight - 40, 20, RED);
             }
         }
-        else
-        {
-            DrawText("Options", screenWidth / 2 - MeasureText("Options", 20) / 2, 360, 20, RED);
-        }
-
-        // Exit Button
-        if (CheckCollisionPointRec(GetMousePosition(), { 0, screenHeight - 40, 200, 50 }))
-        {
-            DrawText("Exit", 20, screenHeight - 40, 40, RED);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            {
-                CloseWindow();
-            }
-        }
-        else
-        {
-            DrawText("Exit", 20, screenHeight - 40, 20, RED);
-        }
+        
 
         DrawText("BARGAIN TEAM @ 2023", screenWidth - 125, screenHeight - 15, 10, WHITE);
         EndDrawing();
